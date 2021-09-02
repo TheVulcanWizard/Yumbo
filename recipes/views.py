258984688a0beb31c5e0a874a2e1b19a8b061c2a
Logan Severin <loganseverin@utexas.edu>
2021-09-01 20:09:23 -0500

    Updated recipe detail headers

--- recipes/views.py
+++ recipes/views.py
@@ -20,10 +20,11 @@ def about(request):
 
 @login_required
 def recipes(request):
+    #creating this view in the event that our home dashboard differs from a recipe focused view
     context = {
         'recipes': Recipe.objects.filter(user=request.user),
     }
-    return render(request, 'recipes/recipes.html', context)
+    return render(request, 'recipes/home.html', context)
 
 @login_required
 def recipe_detail(request, recipe_id):
